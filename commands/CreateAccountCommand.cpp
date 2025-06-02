26c6c31b4a160aba5bea4c2274e872620a3645af
Miroslav Balev <zevs8@abv.bg>
2025-06-02 12:20:19 +0300

    Add create account command

---
+++ commands/CreateAccountCommand.cpp
@@ -0,0 +1,19 @@
+#include "CreateAccountCommand.h"
+
+#include "../services/UsersDatabase.h"
+
+CreateAccountCommand::CreateAccountCommand(const String& username, const String& password):
+    username(username),
+    password(password) {}
+
+void CreateAccountCommand::execute(System& system) {
+    const User* currUser = system.getCurrentUser();
+
+    if (currUser) {
+        throw std::logic_error("Command forbidden!");
+    }
+
+    UsersDatabase usersDb("../users");
+    usersDb.addNewUser(username, password, "User");
+    std::cout << "Account created successfully. Now you can login" << std::endl;
+}
