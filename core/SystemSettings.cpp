9a4b6f2b8c24b5360c020a333d68ecbd78fe8816
Miroslav Balev <zevs8@abv.bg>
2025-06-13 11:40:53 +0300

    Save system settings in file

--- core/SystemSettings.cpp
+++ core/SystemSettings.cpp
@@ -1,5 +1,8 @@
 #include "SystemSettings.h"
 
+#include <fstream>
+#include <stdexcept>
+
 SystemSettings SystemSettings::instance;
 
 SystemSettings& SystemSettings::getInstance() {
@@ -7,11 +10,50 @@ SystemSettings& SystemSettings::getInstance() {
 }
 
 SystemSettings::SystemSettings() {
-    setFileMode(BINARY);
+    FileMode currMode = getFromFile();
+    setFileMode(currMode);
+}
+
+void SystemSettings::saveToFile() const {
+    std::ofstream ofs("../settings.txt");
+    if (!ofs.is_open()) {
+        throw std::runtime_error("Error: could not open settings file");
+    }
+
+    switch (mode) {
+        case TEXT:
+            ofs << "text";
+            break;
+        case BINARY:
+            ofs << "binary";
+            break;
+    }
+
+    ofs.close();
+}
+
+FileMode SystemSettings::getFromFile() const {
+    std::ifstream ifs("../settings.txt");
+
+    if (!ifs.is_open()) {
+        throw std::runtime_error("Error: could not open settings file");
+    }
+
+    String token;
+    ifs >> token;
+
+    if (token == "text") {
+        return TEXT;
+    } else if (token == "binary") {
+        return BINARY;
+    } else {
+        return DEFAULT;
+    }
 }
 
 void SystemSettings::setFileMode(FileMode newMode) {
     mode = newMode;
+    saveToFile();
 }
 
 FileMode SystemSettings::getFileMode() const {
