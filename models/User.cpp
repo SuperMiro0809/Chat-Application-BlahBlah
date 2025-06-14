bbb07c7115fe9e8674260886e0d937e230e4ea69
Miroslav Balev <zevs8@abv.bg>
2025-06-14 18:32:46 +0300

    User has chats vector

--- models/User.cpp
+++ models/User.cpp
@@ -1,5 +1,7 @@
 #include "User.h"
 
+#include "../core/Constants.h"
+
 User::User(unsigned int id, const String& username, const String& password): id(id), username(username), password(password) {}
 
 User::User(unsigned int id, const char* username, const char* password): id(id), username(username), password(password) {}
@@ -12,8 +14,21 @@ const String& User::getUsername() const {
     return username;
 }
 
+bool User::getAreChatsLoaded() const {
+    return areChatsLoaded;
+}
+
 bool User::doPasswordsMatch(const String& checkPass) const {
     return password == checkPass;
+}
+
+void User::loadCriteriaChats() {
+    chats.loadFromFileByCriteria(CHATS_DB_NAME, id);
+    areChatsLoaded = true;
+}
+
+const Vector<Chat>& User::getChats() const {
+    return chats;
 }
 
 UserRole User::getRole() const{
