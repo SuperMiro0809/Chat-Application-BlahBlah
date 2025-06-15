545f6a96a754f9621505ca6e8bbcce10e10d2320
Miroslav Balev <zevs8@abv.bg>
2025-06-15 18:17:59 +0300

    Send message when in chat

--- commands/SelectChatCommand.cpp
+++ commands/SelectChatCommand.cpp
@@ -1,6 +1,7 @@
 #include "SelectChatCommand.h"
 
 #include "../services/ChatsDatabase.h"
+#include "../services/ChatMessagesDatabase.h"
 #include "../core/Constants.h"
 
 SelectChatCommand::SelectChatCommand(unsigned int chatId): chatId(chatId) {}
@@ -35,5 +36,23 @@ void SelectChatCommand::execute(System& system) const {
 
     if (!isChatFound) {
         throw std::logic_error("Chat not found!");
+    }
+
+    std::cout << "Enter message (or type 'no' to cancel): " << std::endl;
+    String message;
+
+    ChatMessagesDatabase chatMessagesDb(MESSAGES_DB_NAME);
+
+    while (true) {
+        std::cout << "> ";
+        std::cin >> std::ws >> message;
+
+        if (message == "no") {
+            std::cout << "Message input cancelled." << std::endl;
+            break;
+        }
+
+        chatMessagesDb.addNewMessage(chatId, currUser->getId(), message);
+        std::cout << "Message sent." << std::endl;
     }
 }
